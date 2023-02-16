da110acb82e934c93de499a25b7a4b11c77a3686
Jkotoun <jkotoun58@gmail.com>
2023-02-16 12:33:10 +0100

    add scripts

--- doc.sh
+++ doc.sh
@@ -1,1 +1,5 @@
+if [ "$1" == "install" -o "$1" == "--install" -o "$1" == "-i" ] 
+then
+    npm install
+fi
 ./node_modules/jsdoc/jsdoc.js -c jsdoc.json iterate.mjs
--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
+if [ "$1" == "install" -o "$1" == "--install" -o "$1" == "-i" ] 
+then
+    npm install
+fi
 npm test
